88bd450f62cc23b08db916d84023ba9149ca2454
bmiit145 <21bmiit145@gmail.com>
2023-08-07 23:54:28 +0530

    init project

---
+++ router.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+// const MessageRouter = require("./routes/sendmessage");
+// const ContactRouter =  require('./routes/contact');
+
+
+// app.use("/msg" , MessageRouter);
+// app.use("/Contact" , ContactRouter);
+
+
+module.exports = app;
