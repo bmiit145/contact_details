830834db90a8663cc23f334a098b8b245a9dbc2e
bmiit145 <21bmiit145@gmail.com>
2023-08-13 13:38:13 +0530

    vercel issue upload folder creation

--- multer.js
+++ multer.js
@@ -1,10 +1,10 @@
 const multer = require('multer')
 const fs = require('fs')
 const uploadsFolderPath = __dirname + '/uploads';
-
-if (!fs.existsSync(uploadsFolderPath)) {
-  fs.mkdirSync(uploadsFolderPath);
-}
+  
+// if (!fs.existsSync(uploadsFolderPath)) {
+//   fs.mkdirSync(uploadsFolderPath);
+// }
 // Multer Setup for file uploads
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
