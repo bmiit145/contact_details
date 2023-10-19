7e5cfe390670b8d30effdea3d25f746c14531f5e
bmiit145 <21bmiit145@gmail.com>
2023-10-19 16:08:33 +0530

    complate for phone

--- controller/contactController.js
+++ controller/contactController.js
@@ -1,93 +1,152 @@
 const fs = require('fs');
 const csvParser = require('csv-parser');
-const { exit } = require('process');
+const {exit} = require('process');
+const axios = require("axios");
 
 module.exports = exports = {
     viewContact: (req, res) => {
         // res.send("646");
         res.render('view_form')
     },
-    saveContact: (req, res) => {
-        // read a data of parser
-        var data = []
-        const csvFilePath = req.file.path;
 
-        fs.createReadStream(csvFilePath)
+    saveContact: (req, res) => {
+        parseCSVFile(req.file.path)
+            .then((phonedata) => {
+                return sendToPythonAPI(phonedata);
+            })
+            .then((response) => {
+                console.log('Response:', response.data);
+                res.status(200).json({ message: 'Data processed successfully' });
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+                res.status(500).json({ error: 'Internal Server Error' });
+            });
+    },
+
+
+    // saveContact: (req, res) => {
+    //     // read a data of parser
+    //     var data = []
+    //     const csvFilePath = req.file.path;
+    //
+    //     fs.createReadStream(csvFilePath)
+    //         .pipe(csvParser())
+    //         .on('headers', (headers) => {
+    //             if (headers.includes('phone') || headers.includes('Phone')) {
+    //                 console.log('CSV file contains the <phone> field.');
+    //
+    //                 fs.createReadStream(csvFilePath)
+    //                     .pipe(csvParser())
+    //                     .on('data', (row) => {
+    //                         data.push(row);
+    //                     })
+    //                     .on('end', () => {
+    //                         console.log('CSV file read and processed.');
+    //                         // console.log(data);
+    //
+    //                         phonedata = data.map(item => ({
+    //                             "phone": getNormalizedPhone(item)
+    //                         }));
+    //
+    //                         // console.log(JSON.stringify(phonedata, null, 2));
+    //                         data = JSON.stringify(phonedata, null, 2);
+    //                         // console.log(data)
+    //
+    //
+    //                         // validating data by python ajax
+    //
+    //                         const axios = require('axios');
+    //
+    //                         const apiUrl = 'https://validate-conatct-json.vercel.app/validate';
+    //
+    //                         // Data to send in the request
+    //                         const postData = {
+    //                             data: data
+    //                         };
+    //
+    //                         // Make a POST request with the data
+    //                         axios.post(apiUrl, postData)
+    //                             .then(response => {
+    //                                 console.log('Response:', response.data);
+    //                             })
+    //                             .catch(error => {
+    //                                 console.error('Error:', error);
+    //                             });
+    //
+    //                     });
+    //             } else {
+    //                 console.log('CSV file not contains the "phone" field.');
+    //                 res.status(400).send('CSV file not  contains the "phone" field.');
+    //             }
+    //         })
+    //         .on('error', (error) => {
+    //             console.error('Error reading CSV file:', error);
+    //             res.status(500).send('Error reading CSV file.');
+    //         });
+    // }
+}
+
+function parseCSVFile(filePath) {
+    return new Promise((resolve, reject) => {
+        const data = [];
+        // allowed only PHONE field
+        const phoneHeaderRegex = /^\s*phone\b\s*$/i;
+
+        // allowed phone with veriations with number
+        // const phoneHeaderRegex = /\bphone\b(?:s?\d?)?\b/i;
+
+        fs.createReadStream(filePath)
             .pipe(csvParser())
             .on('headers', (headers) => {
-                if (headers.includes('phone') || headers.includes('Phone')) {
-                    console.log('CSV file contains the <phone> field.');
-
-                    fs.createReadStream(csvFilePath)
+                if (headers.some(header => phoneHeaderRegex.test(header))) {
+                    console.log('CSV file contains the "phone" field.');
+                    fs.createReadStream(filePath)
                         .pipe(csvParser())
                         .on('data', (row) => {
                             data.push(row);
                         })
                         .on('end', () => {
                             console.log('CSV file read and processed.');
-                            console.log(data);
-
-                            phonedata = data.map(item => ({
-                                "phone": getNormalizedPhone(item)
+                            const phonedata = data.map(item => ({
+                                phone: getNormalizedPhone(item),
                             }));
-
-                            console.log(JSON.stringify(phonedata, null, 2));
-
+                            resolve(phonedata);
                         });
                 } else {
-                    console.log('CSV file not contains the "phone" field.');
-                    res.status(400).send('CSV file not  contains the "phone" field.');
+                    console.log('CSV file does not contain the "phone" field.');
+                    reject('CSV file does not contain the "phone" field.');
                 }
             })
             .on('error', (error) => {
                 console.error('Error reading CSV file:', error);
-                res.status(500).send('Error reading CSV file.');
+                reject('Error reading CSV file.');
             });
-
-        // const data1 = [
-        //     { Name: 'K595', Phone: '+265 883 65 68 59' },
-        //     { Name: 'K596', Phone: '+265 994 26 00 10' },
-        //     { Name: 'K597', Phone: '+91 6353 009 487' },
-        //     { Name: 'K598', Phone: '+91 6353 605 596' },
-        //     { Name: 'K599', Phone: '+91 6354 880 802' },
-        //     { Name: 'K600', Phone: '+91 6355 050 757' },
-        //     { Name: 'K601', Phone: '+91 70046 81515' }
-        //   ];
-
-
-
-        // validating data by python ajax
-
-        // const axios = require('axios');
-
-        // const apiUrl = 'https://validate-conatct-json.vercel.app/validate';
-
-        // // Data to send in the request
-        // const postData = {
-        //     data: data
-        // };
-
-        // // Make a POST request with the data
-        // axios.post(apiUrl, postData)
-        //     .then(response => {
-        //         console.log('Response:', response.data);
-        //     })
-        //     .catch(error => {
-        //         console.error('Error:', error);
-        //     });
-    }
+    });
 }
-
 
 // Function to normalize phone attribute's case
 function getNormalizedPhone(item) {
-    const phoneAttributes = ["phone", "Phone", "PHonE", "PHONE"]; // Add more variations as needed
-
-    for (const attr of phoneAttributes) {
-        if (item.hasOwnProperty(attr)) {
-            return item[attr].toLowerCase();
+    const PhoneRegex = new RegExp('phone', "i");
+    for (const key in item) {
+        if (PhoneRegex.test(key)) {
+            return item[key].toLowerCase();
         }
     }
-
     return null; // Handle the case where phone attribute is not found
+}
+
+function sendToPythonAPI(phonedata) {
+    const apiUrl = 'https://validate-conatct-json.vercel.app/validate';
+
+    // Create an array of objects with the "phone" attribute in the format you want
+    const formattedData = phonedata.map(item => ({
+        phone: item.phone,
+    }));
+
+    const postData =  formattedData ;
+
+    // console.log('Sending data to Python API:', postData)
+
+    return axios.post(apiUrl, postData);
 }
